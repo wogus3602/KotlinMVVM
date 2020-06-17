8e68f4b8a1e4dc13e677a90247260fd7f72c74ef
park <wogus3602@naver.com>
2020-06-17 17:14:16 +0900

    add retrofit2

--- app/src/main/java/com/example/myapplication/di/MyModule.kt
+++ app/src/main/java/com/example/myapplication/di/MyModule.kt
@@ -1,15 +1,32 @@
 package com.example.myapplication.di
 
+import com.example.myapplication.model.DataModelImpl
 import com.example.myapplication.viewmodel.MainViewModel
 import org.koin.dsl.module.module
-import com.example.myapplication.viewmodel.MainViewModel.DataModelImpl
 import org.koin.androidx.viewmodel.ext.koin.viewModel
+import com.example.myapplication.model.DataModel
+import com.example.myapplication.model.service.KakaoSearchService
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
 
 //의존성 주입
+var retrofitPart = module{
+    single<KakaoSearchService> {
+        Retrofit.Builder()
+            .baseUrl("https://dapi.kakao.com")
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(KakaoSearchService::class.java)
+    }
+}
 
 var modelPart = module {
-    factory <MainViewModel.DataModel> {
-        DataModelImpl()
+    factory <DataModel> {
+        DataModelImpl(get())
     }
 }
 
@@ -20,6 +37,7 @@ var viewModelPart = module {
 }
 
 var myDiModule = listOf(
+    retrofitPart,
     modelPart,
     viewModelPart
 )
